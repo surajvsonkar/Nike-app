09ec9daa6922e1bb84c917bd4d10e3ec3e9ba9e1
Suraj  Sonkar <surajvirendrasonkar@gmail.com>
2024-12-28 00:59:32 +0530

    services section done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,9 +4,9 @@ const Button = (props) => {
   return (
     <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white '>
     {props.label}
-    <img src={props.iconURL} alt='arrow right icon'
+    {props.iconURL && <img src={props.iconURL} alt='arrow right icon'
         className='ml-2 rounded-full w-5 h-5'
-    />
+    />}
 </button>
   )
 }
---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,13 @@
+const ServiceCard = (props) => {
+  return (
+    <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16">
+        <div className="w-11 h-11 rounded-full flex bg-coral-red justify-center items-center">
+            <img src={props.imgURL} alt={props.name} width={24} height={24} />
+        </div>
+        <h3 className="mt-5 font-palanquin text-3xl leading-normal font-bold">{props.label}</h3>
+        <p className="mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray">{props.subtext}</p>
+    </div>
+  )
+}
+
+export default ServiceCard
