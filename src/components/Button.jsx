aa014476146e202bf1e60c37ff9b42a74833a47d
Suraj  Sonkar <surajvirendrasonkar@gmail.com>
2025-01-04 23:48:10 +0530

    special offer done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white '>
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white`}>
     {props.label}
     {props.iconURL && <img src={props.iconURL} alt='arrow right icon'
-        className='ml-2 rounded-full w-5 h-5'
-    />}
-</button>
+        className='ml-2 rounded-full w-5 h-5'/>}
+    </button>
   )
 }
 
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { offer } from '../assets/images'
 import Button from '../components/Button'
+import arrowRight from '../assets/icons/arrow-right.svg'
 
 const SpecialOffer = () => {
   return (
@@ -10,20 +11,17 @@ const SpecialOffer = () => {
       </div>
       <div className="flex flex-1 flex-col">
 				<h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
-					We Provide You
-					<span className="text-coral-red"> Super </span>
-					<span className="text-coral-red">Quality </span>Shoes
+					<span className="text-coral-red"> Special </span>offer
 				</h2>
 				<p className="mt-4 lg:max-w-lg info-text">
-					Ensuring premium comfort and style, our meticulously crafted shoes are
-					designed to elevate your experience, prociding you with unmatched
-					quality, innovation and a touch or elegance.
+					Embark on a shopping journey that redefines your experience with unbeatable deals from premier selections to incredible savings. we offer unparalleled value that sets us apart.
 				</p>
 				<p className="mt-6 lg:max-w-lg info-text">
-					Our dedication to detail and excellence ensures your satisfaction
+					Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of exceptional.
 				</p>
-				<div className="mt-11">
-					<Button label="View Details" />
+				<div className="mt-11 flex flex-wrap gap-4">
+					<Button label="Shop now" iconURL={arrowRight} />
+					<Button label="Learn more" backgroundColor="bg-white" borderColor="border-slate-gray" textColor="text-slate-gray" />
 				</div>
 			</div>
     </section>
