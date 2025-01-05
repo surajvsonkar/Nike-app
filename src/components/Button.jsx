1e55a8c1e996f05ad8a163710670287ccf49092e
Suraj  Sonkar <surajvirendrasonkar@gmail.com>
2025-01-05 11:10:28 +0530

    button updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const Button = (props) => {
-  return (
-    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white`}>
-    {props.label}
-    {props.iconURL && <img src={props.iconURL} alt='arrow right icon'
-        className='ml-2 rounded-full w-5 h-5'/>}
-    </button>
-  )
-}
+const Button = ({
+	label,
+	iconURL,
+	backgroundColor,
+	textColor,
+	borderColor,
+}) => {
+		return (
+			<button
+				className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+				${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}`: "bg-coral-red text-white border-coral-red"} rounded-full w-full"}`}>
+				{label}
+				{iconURL && (
+					<img
+					src={iconURL}
+					alt="arrow right icon"
+					className="ml-2 rounded-full w-5 h-5"
+					/>)} 
+			</button>
+		)
+	};
 
-export default Button
+export default Button;
